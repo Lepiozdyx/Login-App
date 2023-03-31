7e54d199cfbc5740107a1b73a892b73789b3fb3a
Alex Zhulai <Lepiozdyx@gmail.com>
2023-03-31 11:23:39 -0300

    Add a second class WelcomeViewController

---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by Alex on 31.03.2023.
+//
+
+import UIKit
+
+final class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+}
