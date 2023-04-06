e0dad50bebfb657283beee5d9349325e449f649b
Alex Zhulai <Lepiozdyx@gmail.com>
2023-04-06 11:57:59 -0300

    Change Models structure

--- LoginApp/Models/Person.swift
+++ LoginApp/Models/Person.swift
@@ -6,42 +6,90 @@
 //
 
 struct User {
-    let userName = "Alex"
-    let password = "Pass"
+    let userName: String
+    let password: String
     let person: Person
     
     static func getUser() -> User {
-        User(person: Person())
+        User(
+            userName: "Alexey",
+            password: "Pass",
+            person: Person(
+                photo: "photo",
+                firstName: "Alex",
+                lastName: "July",
+                age: "33",
+                hobbies: "Computer technologies",
+                maritalStatus: "Married"
+            )
+        )
     }
 }
 
 struct Person {
-    let photo = "photo"
-    let firstName = "Alex"
-    let lastName = "Zhulai"
-    let age = "33"
-    let hobbies = "Videogames, basketbol, guitars"
-    let maritalStatus = "Married"
+    let photo: String
+    let firstName: String
+    let lastName: String
+    let age: String
+    let hobbies: String
+    let maritalStatus: String
     let biography = """
-Hi! Glad to welcome you to my app, I want to express my gratitude for the information you share with us in your lectures, and most importantly, how you do it! I believe that this IT marathon will end up being productive for many of us!
-If you want, I'll tell you a little bit about myself. My name is Alexander, I live in Argentina, in my previous life I was an engineer at 'Lukoil' deep in the north of Russia, and I can still remember how I opened Vasily Usov's book on my computer an hour before work time ended, and read about the basics of Swift programming, and, like everyone at my age, was afraid to make a step towards the new. Now I'm listening to your lectures from the opposite side of the planet and firmly believe to go this way!
-I'm also a fan of Tolkien's work, love the Star Wars universe (except for the last three misunderstandings, not counting Rogue One, it's okay :)).
+Is this the real life?
+Is this just fantasy?
+Caught in a landside,
+No escape from reality
+Open your eyes,
+Look up to the skies and see,
+I'm just a poor boy, I need no sympathy,
+Because I'm easy come, easy go,
+Little high, little low,
+Any way the wind blows doesn't really matter to
+Me, to me
+Mamaaa,
+Just killed a man,
+Put a gun against his head, pulled my trigger,
+Now he's dead
+Mamaaa, life had just begun,
+But now I've gone and thrown it all away
+Mama, oooh,
+Didn't mean to make you cry,
+If I'm not back again this time tomorrow,
+Carry on, carry on as if nothing really matters
+Too late, my time has come,
+Sends shivers down my spine, body's aching all
+The time
+Goodbye, everybody, I've got to go,
+Gotta leave you all behind and face the truth
+Mama, oooh
+I don't want to die,
+I sometimes wish I'd never been born at all.
+I see a little silhouette of a man,
+Scaramouch, Scaramouch, will you do the Fandango!
+Thunderbolts and lightning, very, very frightening me
+Galileo, Galileo
+Galileo, Galileo
+Galileo, Figaro - magnificoo
+I'm just a poor boy nobody loves me
+He's just a poor boy from a poor family,
+Spare him his life from this monstrosity
+Easy come, easy go, will you let me go
+Bismillah! No, we will not let you go
+(Let him go!) Bismillah! We will not let you go
+(Let him go!) Bismillah! We will not let you go
+(Let me go) Will not let you go
+(Let me go)(Never) Never let you go
+(Let me go) (Never) let you go (Let me go) Ah
+No, no, no, no, no, no, no
+Oh mama mia, mama mia, mama mia, let me go
+Beelzebub has a devil put aside for me, for me,
+For meee
+So you think you can stop me and spit in my eye
+So you think you can love me and leave me to die
+Oh, baby, can't do this to me, baby,
+Just gotta get out, just gotta get right outta here
+Nothing really matters, Anyone can see,
+Nothing really matters,
+Nothing really matters to me
+Any way the wind blows...
 """
 }
-
-//enum Hobby {
-//    case sport
-//    case films
-//    case music
-//
-//    var definition: String {
-//        switch self {
-//        case .sport:
-//            return "Basketball"
-//        case .films:
-//            return "Lord of the Rings and Star Wars"
-//        case .music:
-//            return "60-90's Rock music"
-//        }
-//    }
-//}
